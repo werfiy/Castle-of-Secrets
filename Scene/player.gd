c6cef5d1c3af42d70c119db117574a5476ff9557
werfiy <134719058+werfiy@users.noreply.github.com>
2024-11-07 21:47:03 +1000

    atack for E and skeleton script (NOT WORK)

--- Scene/player.gd
+++ Scene/player.gd
@@ -5,7 +5,21 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 @onready var anim = $AnimatedSprite2D
+var is_atacking = false
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Atackfunc1") and is_on_floor:
+		if not is_atacking:
+			is_atacking = true
+			anim.play("Atack1")
+
+func _on_AtackAnimation_finished():
+	is_atacking = false
+
+func _on_Area2D_body_entered(body: Node):
+	if is_atacking and body.is_in_group("enemies"):
+		body.queue_free()
+		 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -17,7 +31,7 @@ func _physics_process(delta: float) -> void:
 		anim.play("jump")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		if velocity.y == 0:
@@ -33,5 +47,8 @@ func _physics_process(delta: float) -> void:
 	
 	if velocity.y > 0:
 		anim.play("fall")
+
+
+
 
 	move_and_slide()
---
+++ Scene/skeleton.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var chase = false
+var speed = 100
+@onready var anim = $AnimatedSprite2D
+var alive = true
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	var player = $"../Player"
+	var direction = (player.position - self.position).normalized()
+	if chase == true:
+		velocity.x = direction.x * speed
+		anim.play("walk")
+	else:
+		velocity.x = 0
+		anim.play("idle")
+	move_and_slide()
+	if direction.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
+
+
+func _on_detector_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = true
+
+
+func _on_detector_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = false
+		
+func death ():
+	alive = false
+	anim.play("death")
+	await anim.animation_finished
+	queue_free()
